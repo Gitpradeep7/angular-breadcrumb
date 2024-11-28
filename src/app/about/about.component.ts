de918ac733364c510610441c2c65c742b3a818d0
Gitpradeep7 <pradeepyellamelli0@gmail.com>
2024-11-28 09:24:34 +0530

    added the children component

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -7,28 +7,45 @@ import { Component } from '@angular/core';
 })
 export class AboutComponent {
   names = ["Alice", "Bob", "Charlie", "Diana"];
-  
-  dupIndex: any[]=[];
+  index:any[] = [];
+  dupIndex: tab={
+    num:0,
+    count:0
+  };
   numbers = [472, 486, 869, 195, 975, 539, 345, 143, 328, 622, 732, 88, 261, 415, 938, 234, 316,
     796, 592, 664, 597, 592, 570, 10, 66, 139, 52, 611, 821, 381, 900, 558, 432, 589, 396, 592,
     942, 592, 809, 265, 23, 765, 41, 436, 714, 592, 616, 843, 862, 7, 592, 629, 113, 102, 592,
     663, 235, 34, 290, 739, 312, 592, 173, 779, 598, 310, 529, 637, 127, 698, 969, 672, 126,
     357, 295, 5, 376, 343, 769, 224, 469, 673, 870, 774, 592, 700, 592, 480, 556, 639, 227,
-    465, 678, 270, 697, 4, 980, 320, 563, 592];
+    465, 678, 270, 697, 4, 980, 320, 563, 592,11];
     constructor(){}
     ngOnInit(){
-
       this.getIndexOfDup();
     }
   getIndexOfDup() {
-  
-   this.dupIndex = this.numbers.filter((i:any,ind:any)=>{
-      i==this.numbers[ind] ? ind : '';
-    })
-    console.log("ddd",this.dupIndex)
+    let dup: number[]=[];
+    for(let i of this.numbers){
+      let count:number=0;
+      for(let j of this.numbers){
+        if(i==j){
+          count++;
+        }
+      }
+      // let n = dup.every((nu:any)=> nu==i);
+      // console.log('n',n)
+    if(!dup.includes(i)){
+      dup.push(i);
+      this.dupIndex.count = count;
+      this.dupIndex.num = i;
+      this.index.push(this.dupIndex)
+    }
+    }
+    let ar = this.dupIndex.num;
+    console.log('ar',ar)
+    console.log(this.index)
   }
 }
 export interface tab{
   num:number,
-  index:number
+  count:number
 }
